eac6cbecde51d580345a7ee60f1d6de4ffc4be35
Marcel-Makowski-203944 <marcelekmakowski@gmail.com>
2026-01-17 17:07:59 +0100

    Create PROJEKT2.py

---
+++ PROJEKT2.py
@@ -0,0 +1,4 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import Qt, QTimer, QPointF
+from PyQt5.QtGui import QPainter, QColor, QPen, QPainterPath
