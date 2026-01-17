dc1ed4051a1f5751f3dff247cc59bc5580aae030
Marcel-Makowski-203944 <marcelekmakowski@gmail.com>
2026-01-17 17:25:57 +0100

    Update PROJEKT2.py

--- PROJEKT2.py
+++ PROJEKT2.py
@@ -31,3 +31,54 @@ class Rura:
             pen_ciecz = QPen(self.kolor_cieczy, self.grubosc - 4, Qt.SolidLine,Qt.RoundCap, Qt.RoundJoin)
             painter.setPen(pen_ciecz)
             painter.drawPath(path)
+            
+class Zbiornik :
+    def __init__ ( self , x , y , width =100 , height =140 , nazwa ="") :
+        self . x = x ; self.y = y
+        self . width = width ; self.height = height
+        self . nazwa = nazwa
+        self . pojemnosc = 100.0
+        self . aktualna_ilosc = 0.0
+        self . poziom = 0.0
+    def dodaj_ciecz ( self , ilosc ) :
+        wolne = self . pojemnosc - self . aktualna_ilosc
+        dodano = min( ilosc , wolne )
+        self . aktualna_ilosc += dodano
+        self . aktualizuj_poziom ()
+        return dodano
+    def usun_ciecz ( self , ilosc ) :
+        usunieto = min( ilosc , self.aktualna_ilosc )
+        self . aktualna_ilosc -= usunieto
+        self . aktualizuj_poziom ()
+        return usunieto
+    def aktualizuj_poziom ( self ) :
+        self . poziom = self . aktualna_ilosc / self . pojemnosc
+    def czy_pusty ( self ) :
+        return self . aktualna_ilosc <= 0.1
+    def czy_pelny ( self ) :
+        return self . aktualna_ilosc >= self . pojemnosc - 0.1
+
+    def punkt_gora_srodek(self):
+        return (self.x + self.width / 2, self.y)
+
+
+
+    def punkt_dol_srodek(self):
+        return (self.x + self.width / 2, self.y + self.height)
+
+
+
+    def draw(self, painter):
+        if self.poziom > 0:
+            h_cieczy = self.height * self.poziom
+            y_start = self.y + self.height - h_cieczy
+            painter.setPen(Qt.NoPen)
+            painter.setBrush(QColor(0, 120, 255, 200))
+            painter.drawRect(int(self.x + 3), int(y_start), int(self.width - 6),int(h_cieczy - 2))
+        pen = QPen(Qt.white, 4)
+        pen.setJoinStyle(Qt.MiterJoin)
+        painter.setPen(pen)
+        painter.setBrush(Qt.NoBrush)
+        painter.drawRect(int(self.x), int(self.y), int(self.width), int(self.height))
+        painter.setPen(Qt.white)
+        painter.drawText(int(self.x), int(self.y - 10), self.nazwa)
